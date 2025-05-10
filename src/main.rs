cf0bec1e626110e15a675dd465230bd16dc5c74e
1Terabit <54971252+1Terabit@users.noreply.github.com>
2025-05-10 02:46:58 -0400

    feat(update_checker): add update checker module and integrate into main (#8) (#9)
    Add a new module `update_checker` to check for the latest version of the CLI from crates.io. Integrate the update checker into the main function to notify users of available updates. Update dependencies and version to support this feature.

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,40 @@ mod cli;
 mod handlers;
 mod utils;
 
-use cli::{Cli, Commands, MakeItem};
+use crate::cli::update_checker::UpdateChecker;
 use clap::Parser;
+use cli::{Cli, Commands, MakeItem};
 use handlers::new::NewCommand;
 
 fn main() {
     let cli = Cli::parse();
-
     match cli.command {
-        Commands::New { name, dependencies, options, no_interactive } => {
+        Commands::New {
+            name,
+            dependencies,
+            options,
+            no_interactive,
+        } => {
             let command = NewCommand::new(&options);
-            command.execute(&name, dependencies, no_interactive).unwrap();
+            if let Err(e) = command.execute(&name, dependencies, no_interactive) {
+                eprintln!("Error creating new project: {}", e);
+                std::process::exit(1);
+            }
         }
         Commands::Make { item } => match item {
             MakeItem::Route { name, method } => handlers::make::create_route(&name, method),
             MakeItem::Model { name } => handlers::make::create_model(&name),
-            MakeItem::Service { name } => handlers::make::create_component("services", &name, "💼 Service", "service"),
-            MakeItem::Core { name } => handlers::make::create_component("core", &name, "🧠 Core logic", "core"),
+            MakeItem::Service { name } => {
+                handlers::make::create_component("services", &name, "💼 Service", "service")
+            }
+            MakeItem::Core { name } => {
+                handlers::make::create_component("core", &name, "🧠 Core logic", "core")
+            }
         },
         Commands::Usecase { name } => handlers::usecase::create_usecase(&name),
+    }
+    let checker = UpdateChecker::new();
+    if let Err(e) = checker.check_for_updates() {
+        eprintln!("Error checking for updates: {}", e);
     }
 }
